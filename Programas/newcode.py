faf704547863e39c001e568f0ca988bd72e17bb2
Induction <inductionv8@gmail.com>
2015-04-15 13:06:34 -0300

    added fix in newcode

--- Programas/newcode.py
+++ Programas/newcode.py
@@ -1,13 +1,23 @@
 for x in range(1, 2):
     #Abro el archivo x-esimo
     print("Archivo numero "+ str(x))
-    archivo = open("summary" + str(x) + ".txt")
+    archivo = open("summary.txt")
     #Leo la linea
     line = archivo.readline()
+    temp = []
     while line:
         _list = line.split("\t")
-        if not _list:
-            print("\n")
-        else:
-            print(_list[5])
+        if _list:
+            palabra = _list[0]
+            if palabra[7].isdigit() == True:
+                temp.append([palabra[6:8], _list[5]])
+            else:
+                temp.append([palabra[6], _list[5]])
             line = archivo.readline()
+    archivo.close()
+    temp = temp[1:]
+    temp.sort(key=lambda temp: temp[-1])
+    for imagen in temp:
+        print("Imagen "+imagen[0])
+        print("D: "+imagen[1])
+        print("\n")
